0d83fb93bf10e3fd239339cbd51c0bb8ed4e6e8c
sarah <sarahabimay@gmail.com>
2015-07-02 16:23:07 +0100

    check for win after each move

--- tictactoe.js
+++ tictactoe.js
@@ -24,60 +24,102 @@ var BoardGame = (function(){
 			board[ position ] = counter;
 		}
 
+		function foundPositionToBlock( counter ) {
+			// search for any possible winning scenarios for counter.
+			// return the position that could block that win.
+
+		}
+
+		function found3InARow ( counter ) {
+			if( ( board[ 0 ] === counter && board[ 1 ] === counter && board[ 2 ] === counter ) ||
+				  ( board[ 3 ] === counter && board[ 4 ] === counter && board[ 5 ] === counter ) ||
+				  ( board[ 6 ] === counter && board[ 7 ] === counter && board[ 8 ] === counter ) ||
+				  ( board[ 0 ] === counter && board[ 3 ] === counter && board[ 0 ] === counter ) ||
+				  ( board[ 1 ] === counter && board[ 4 ] === counter && board[ 7 ] === counter ) ||
+				  ( board[ 2 ] === counter && board[ 5 ] === counter && board[ 8 ] === counter ) ||
+				  ( board[ 0 ] === counter && board[ 4 ] === counter && board[ 8 ] === counter ) ||
+				  ( board[ 2 ] === counter && board[ 4 ] === counter && board[ 6 ] === counter ) ) {
+		   			alert( counter + ' has won the game. Start a new game');
+		   		return true;
+			}
+		}
+		function checkForGameOver(){
+			if( playCount < 3) {
+				return false;
+			}
+			if( found3InARow( "X" ) || found3InARow( "O" ) ) {
+				return true;
+			}
+			else if ( playCount === 9 ) {
+				alert(  "It's a DRAW!! Start a new game");
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
 		function incPlayCount(){
 			return playCount++;
 		}
- 
- 		function newGame() {
- 			isPlayer1 = undefined;
- 			board = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
- 		}
 
- 		function isPlayer1Computer() {
- 			return ( isPlayer1 === "Computer" ) ? true : false;
- 		}
-    return {
+		function newGame() {
+			isPlayer1 = undefined;
+			board = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+		}
 
-      // Public methods and variables
-      isPlayer1Computer : function() {
-      	return isPlayer1Computer();
-      },
-      resetGame : function(){
-      	that.newGame();
-      	console.log( "isPlayer1: " + isPlayer1);
-      	console.log( "board: " + board);
-      },
-      updatePlayCount : function (){
-      	return incPlayCount();
-      },
-      updateBoardWithMove : function( position, counter ) {
-      	updateBoard( position, counter );
-      },
-      getCounter : function( isComputer ){
-				if( isComputer ) {
-					return isPlayer1Computer() ? "X" : "O";
-				}
-				else {
-					return isPlayer1Computer() ? "O" : "X";
-				}
-			},
-      isPositionEmpty : function ( position ) {
+		function isPlayer1Computer() {
+			return ( isPlayer1 === "Computer" ) ? true : false;
+		}
+
+		function getCounter( isComputer ){
+			if( isComputer ) {
+				return isPlayer1Computer() ? "X" : "O";
+			}
+			else {
+				return isPlayer1Computer() ? "O" : "X";
+			}
+		}
+
+		return {
+
+		  // Public methods and variables
+		  isPlayer1Computer : function() {
+		  	return isPlayer1Computer();
+		  },
+		  resetGame : function(){
+		  	newGame();
+		  },
+		  getCounter : function (isComputer) {
+		  	return getCounter(isComputer);
+		  },
+
+		  positionToBlock : function() {
+		  	var counter = isPlayer1Computer() ? "O": "X";
+		  	return foundPositionToBlock( counter );
+		  },
+			checkForGameOver : function( position, isComputer ) {
+		  	updateBoard( position, getCounter( isComputer ) );
+		  	incPlayCount();
+		  	return checkForGameOver();
+		  },
+		  
+		  isPositionEmpty : function ( position ) {
 				if( board[ position ] === "X" || board[ position ] === "Y" ){
 					return false;
 				}
 				return true;
 			},
 
-			getUnfilledPositions : function (){
+			getUnfilledSpaces : function (){
 				var fieldsWithoutXorO = board.filter( function( value, index) {
 					return ( value === "X" || value === "O" ) ? false : true;
 				});
 				console.log( 'Unfilled Positions: ' + fieldsWithoutXorO );
 				return fieldsWithoutXorO;
 			}
-    };
-
-  }
+	  };
+	}
 
 	return {
 
@@ -117,64 +159,68 @@ function resetGame (){
 	$("#playerselect").show();
 }
 
-function updateGameBoard ( position, counter ){
+function updateGameBoard ( game, position, isComputer ){
+	// This function will place an X or O on the button element with id equivalent to 'position'.
+	// The html game board has id's which are numbered 1 to 9 in roman numerals so array 'romans'
+	// maps roman numerals to numbers.
+	var counter = game.getCounter( isComputer );
 	var field = $( "#" + domStuff.romans[ position ] );
 	$( ".gameboard" ).find( field ).text( counter );
 	$( ".gameboard" ).find( field ).attr( "disabled", true );
+	field.addClass( "disable");
 }
 
-function playComputerMove( game, position ) {
-	// The html game board has id's which are numbered 1 to 9 in roman numerals so I'm going to need
-	// to map romans to numbers
-	// This function will place an X or O on the button element with id equivalent to 'position'.
-	// We also need to 'cache' the position in the global (for now) 'board' array to keep track of what spaces are still free.  
-	// This is just a global var for now and will only be changed in this function or when the game is restarted.
+function playComputerMove ( game ) {
+	// We also need to 'cache' the position in BoardGame to keep track of what spaces are still free.  
 	
-	// update cache
-	var counter = game.getCounter( true );
-	game.updateBoardWithMove( position - 1, counter);
-
+	var position = generateComputerMove(game);
 	// update DOM
-	updateGameBoard( position -1, counter );
-}
-function badChoice( position ) {
-	// if x and y are found in the badChoices list then return true
-	var badChoices = [ 2, 4, 6, 8 ];
-	
-	var isBad = badChoices.indexOf( position ) !== -1;
-	console.log( isBad ? "Bad Choice" : "Good Choice");
-	return isBad;
+	updateGameBoard( game, position -1, true );
+
+	// update cache
+	var result = game.checkForGameOver( position - 1, true );
+
 }
 
-function generateComputerOpeningMove (game) {
+function badChoice ( position, game ) {
+	// We want the computer to always win or draw so we should only use center or edge positions unless there
+	// is no other option.
+	// In order to ensure this, the function badChoice will tell us if a edge has been selected 
+	// when a non-edge is available.
+	var edgeSpaces = [ 2, 4, 6, 8 ];
+	var isEdge = false;
+	var unfilledSpaces = game.getUnfilledSpaces();
+	// This is how I'm checking if there are only the edge spaces left:
+	// I take the unfilledSpaces array and filter out any edge spaces.
+	// If the resulting array is empty then there is nothing but edge spaces left.
+	// In this case the move/position should be allowed.
+	// If the resulting array is not empty then there are alternative positions and we'd 
+	// want to play one of those first.
+	var nonEdgeSpaces = unfilledSpaces.filter( function(val) { return edgeSpaces.indexOf( val ) < 0; });
+	if( nonEdgeSpaces.length ) {
+		isEdge = edgeSpaces.indexOf( position ) !== -1;
+	}
+	return isEdge;
+}
+
+function generateComputerMove (game) {
 	// Board array is like this: [  1, 2, 3, 4, 5, 6, 7, 8, 9 ];
 	// Where 1 is the top, left corner on the tictactoe board, 3 is top, right corner etc..
 	// Player1 is 'X'
 	
-	// The first time this function is called xpos and ypos will be undefined and the function
+	// The first time this function is called position will be undefined and the function
 	// will randomly choose a position.
-	// We want the computer to always win or draw so we should only use center or edge positions.
-	// In order to ensure this, the function badChoices will tell us if a 'bad' choice (or non-edge or non-center)
-	// position has been generated.  If so then recursively generate another y position and test with badChoice again.
-	var unfilledPositions = game.getUnfilledPositions();
-	position = unfilledPositions[Math.floor(Math.random() * unfilledPositions.length)];
-	
-	console.log( "position: " + position );
-  return ( badChoice( position )) ? generateComputerOpeningMove (game) : position;
-}
-function generateComputerPlay ( game ) {
-	// Board array is like this: [  1, 2, 3, 4, 5, 6, 7, 8, 9 ];
-	// Where 1 is the top, left corner on the tictactoe board, 3 is top, right corner etc..
+	// If position a 'bad' choice then recursively generate another position and test with badChoice again.
 
-	// Check if computer is player1 e.g. X or player2 e.g. O
-	// Randomly pick a position on the board.
-	// If the position is one of the 'bad' choices then choose another position
-	var unfilledPositions = game.getUnfilledPositions();
-	// if( unfilledPosition.length === )
-	position = unfilledPositions[Math.floor(Math.random() * unfilledPositions.length)];
+	// First check if we need to block the other player's win.
+	var position = game.positionToBlock();
+	if( position.length ) return position[ 0 ];
 
-  return ( badChoice( position )) ? generateComputerPlay(game) : position;
+	var unfilledSpaces = game.getUnfilledSpaces();
+	position = unfilledSpaces[Math.floor(Math.random() * unfilledSpaces.length)];
+  return ( badChoice( position, game )) ? generateComputerMove (game) : position;
 }
+
 $(document).ready (function(){
 	
 	$("#player1").click (function(){
@@ -190,7 +236,6 @@ $(document).ready (function(){
   });
 
 	$(".readytoplay").click (function() {
-		var openingMove = [];
 		var game = BoardGame.getInstance();
 
 		var p1Text = "Player1: " + ( game.isPlayer1Computer() ? "Computer" : "You");
@@ -202,9 +247,9 @@ $(document).ready (function(){
 		$("#stateofplay").show();
 
 		if( game.isPlayer1Computer() ) {
-			openingMove = generateComputerOpeningMove(game);
-			console.log( "Generate opening move: " + openingMove);
-			playComputerMove( game, openingMove );
+			if( playComputerMove( game ) ){
+				console.log( "Someone has won" );
+			}
 		}
 		enableGame();
 	});
@@ -223,12 +268,24 @@ $(document).ready (function(){
 		var game = BoardGame.getInstance();
 		var thisID = $(this).attr('id');
 		var position = domStuff.romans.indexOf( thisID );
+		
 		if( game.isPositionEmpty( position )){
-			//update cache
-			var counter = game.getCounter( false );
-			game.updateBoardWithMove( position, counter);
+
 			// update DOM
-			updateGameBoard( position, counter );
+			updateGameBoard( game, position, false );
+
+			//update cache
+			if( game.checkForGameOver( position, false ) ) {
+				console.log( "Someone has won");
+				resetGame();
+			}
+			else {
+				// trigger computer move
+				if( playComputerMove( game ) ){
+					console.log( "Someone has won" );
+					resetGame();
+				}
+			}
 		}
 	});
 });
